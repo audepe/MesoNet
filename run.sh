99eaf1d112a5369ef110ec974947bc65372e6558
audepe <danieldelgadoperera@gmail.com>
2020-04-29 19:14:05 +0100

    Update files to run on docker

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-apt update && apt install -y cmake python3-pip
-pip3 install -r /usr/local/Mesonet/requirements.txt
-python3 /usr/local/Mesonet/example.py
+#!/bin/bash
+apt update && apt install -y cmake python3-pip ffmpeg
+pip3 install -r ./requirements.txt
+python3 ./example.py
